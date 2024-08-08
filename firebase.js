7ea4796e056391858b7df846222180d615c971fd
Phan Anh <phananh12042003@gmail.com>
2024-08-08 15:32:46 -0400

    fix

--- firebase.js
+++ firebase.js
@@ -3,12 +3,12 @@ import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBO0A1zMplReuLVX25fxtFDbLe4TLbwBXM",
+  apiKey: process.env.FIREBASE_API_KEY,
   authDomain: "pantry-tracker-17bbf.firebaseapp.com",
   projectId: "pantry-tracker-17bbf",
   storageBucket: "pantry-tracker-17bbf.appspot.com",
   messagingSenderId: "384214227358",
-  appId: "1:384214227358:web:51622a0998440f18995e8f",
+  appId: process.env.FIREBASE_APP_ID,
   measurementId: "G-THLW5JB5TN",
 };
 
